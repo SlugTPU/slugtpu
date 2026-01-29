724bb55abb21d03250ea62fae0e6cfd1b393f7a4
Angelina Nguyen <anguy490@ucsc.edu>
2026-01-28 18:26:32 -0800

    bias32

--- rtl/bias.sv
+++ rtl/bias.sv
@@ -4,16 +4,16 @@ module bias(
     input logic clk_i,
     input logic rst_i,
 
-    input logic signed[15:0] bias_i,
+    input logic signed[31:0] bias_i,
     input logic valid_i,
-    input logic signed [15:0] data_i,
+    input logic signed [31:0] data_i,
     output logic valid_o;
-    output logic signed [15:0] data_o
+    output logic signed [31:0] data_o
 );
 
 always @(posedge clk_i) begin
     if (rst_i) begin
-        data_o <= 16'b0;
+        data_o <= 32'b0;
         valid_o <= 1'b0;
     end
     else if (valid_i) begin 
