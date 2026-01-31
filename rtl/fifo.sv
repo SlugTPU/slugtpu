2970e8e0fb92f5f6c5c4ff247779a1e4e12ff533
yanghuaxuan <yanghuaxuan@gmail.com>
2026-01-30 16:52:36 -0800

    polish tests + fix fifo + add saved gtk waves

--- rtl/fifo.sv
+++ rtl/fifo.sv
@@ -1,6 +1,6 @@
 module fifo #(
-    parameter DEPTH_P = 7,
-    parameter WIDTH_P = 8
+    parameter int DEPTH_LOG2_P = 3,
+    parameter int WIDTH_P = 8
 ) (
     input clk_i,
     input rst_i,
@@ -14,10 +14,8 @@ module fifo #(
     output logic [0:0] valid_o,
     output logic [0:0] ready_o
 );
-    localparam depth_log2_p = $clog2(DEPTH_P);
-
-    logic [WIDTH_P - 1:0] mem [DEPTH_P];
-    logic [depth_log2_p:0]    wr_ptr_n, wr_ptr_q, rd_ptr_n, rd_ptr_q;
+    logic [WIDTH_P - 1:0] mem [1 << DEPTH_LOG2_P];
+    logic [DEPTH_LOG2_P:0]    wr_ptr_n, wr_ptr_q, rd_ptr_n, rd_ptr_q;
     logic [0:0]               write_en_w, read_en_w;
     wire  [0:0]               is_full_w, is_empty_w;
 
@@ -27,19 +25,14 @@ module fifo #(
     assign rd_ptr_n = (read_en_w) ? rd_ptr_q + 1 : rd_ptr_q;
     assign wr_ptr_n = (write_en_w) ? wr_ptr_q + 1 : wr_ptr_q;
 
-    generate
-    if (DEPTH_P > 1) begin: is_full_empty_deep
-        assign is_full_w = (wr_ptr_q[depth_log2_p] != rd_ptr_q[depth_log2_p]) &&
-                           (wr_ptr_q[depth_log2_p - 1:0] == rd_ptr_q[depth_log2_p - 1:0]);
-        assign is_empty_w = wr_ptr_q == rd_ptr_q;
-    end else begin: is_full_empty_shallow
-       assign is_full_w = (wr_ptr_q != rd_ptr_q);
-       assign is_empty_w = (wr_ptr_q == rd_ptr_q);
-    end
-    endgenerate
+    assign is_full_w = (wr_ptr_q[DEPTH_LOG2_P] != rd_ptr_q[DEPTH_LOG2_P]) &&
+                        (wr_ptr_q[DEPTH_LOG2_P - 1:0] == rd_ptr_q[DEPTH_LOG2_P - 1:0]);
+    assign is_empty_w = wr_ptr_q == rd_ptr_q;
 
    assign ready_o = ~is_full_w;
    assign valid_o = ~is_empty_w;
+
+   assign data_o = mem[rd_ptr_q[DEPTH_LOG2_P - 1:0]];
 
     always_ff @(posedge clk_i) begin
         if (rst_i) begin
@@ -51,26 +44,10 @@ module fifo #(
         end
     end
 
-    generate
-    if (DEPTH_P > 1) begin: data_rw_deep
-        always_ff @(posedge clk_i) begin
-            if (write_en_w) begin
-                mem[wr_ptr_q[depth_log2_p - 1:0]] <= data_i;
-            end
-            if (read_en_w) begin
-                data_o <= mem[rd_ptr_q[depth_log2_p - 1:0]];
-            end
-        end
-    end else begin: data_rw_shallow
-        always_ff @(posedge clk_i) begin
-            if (write_en_w) begin
-                mem[0] <= data_i;
-            end
-            if (read_en_w) begin
-                data_o <= mem[0];
-            end
+    always_ff @(posedge clk_i) begin
+        if (write_en_w) begin
+            mem[wr_ptr_q[DEPTH_LOG2_P - 1:0]] <= data_i;
         end
     end
-    endgenerate
 
 endmodule
--- sim/test_fifo.py
+++ sim/test_fifo.py
@@ -5,6 +5,7 @@ from cocotb.triggers import RisingEdge, ClockCycles, FallingEdge
 from pathlib import Path
 from shared import reset_sequence, clock_start
 from runner import run_test
+from collections import deque
 
 @cocotb.test()
 async def reset_test(dut):
@@ -18,21 +19,54 @@ async def reset_test(dut):
 
 @cocotb.test()
 async def fifo_simple_test(dut):
-    """Test that data propagates through the FIFO"""
+    """Fills up queue and then empties"""
+    depth_log2_p = dut.DEPTH_LOG2_P
     clk_i = dut.clk_i
     rst_i = dut.rst_i
+    ready_o = dut.ready_o
+    ready_i = dut.ready_i
+    valid_i = dut.valid_i
+    data_i = dut.data_i
+    data_o = dut.data_o
+
+    fifo_model = deque(maxlen=2**depth_log2_p.value.to_unsigned())
 
     await clock_start(clk_i)
     await reset_sequence(clk_i, rst_i)
 
-    await ClockCycles(clk_i, 67)
+    await FallingEdge(rst_i)
+    valid_i.value = 0
+    ready_i.value = 0
 
-@pytest.mark.parametrize("depth_p", [1, 7])
-def test_fifo_all(depth_p):
+    await RisingEdge(clk_i)
+    assert ready_o.value == 1, "Empty FIFO should still be ready to accept input even when ready_i == 0"
+
+    # send values
+    for i in range(2 ** depth_log2_p.value.to_unsigned()):
+        await FallingEdge(clk_i)
+        valid_i.value = 1
+        data_i.value = i
+        fifo_model.append(i)
+        await RisingEdge(clk_i)
+
+    # stop sending values
+    await FallingEdge(clk_i)
+    valid_i.value = 0
+    await RisingEdge(clk_i)
+
+    # dequeue
+    for _ in range(2 ** depth_log2_p.value.to_unsigned()):
+        await FallingEdge(clk_i)
+        ready_i.value = 1
+        await RisingEdge(clk_i)
+        assert data_o.value.to_unsigned() == fifo_model.popleft(), "Expected"
+
+@pytest.mark.parametrize("depth_log2_p", [1, 3])
+def test_fifo_all(depth_log2_p):
     proj_path = Path("./rtl").resolve()
     sources = [ proj_path/"fifo.sv" ]
 
     # debug print parameters can be idea if a simulator fails silently without telling you why
     # print(f"DEBUG PARAMETERs: {depth_p}")
 
-    run_test(parameters={"DEPTH_P": depth_p}, sources=sources, module_name="test_fifo", hdl_toplevel="fifo")
+    run_test(parameters={"DEPTH_LOG2_P": depth_log2_p}, sources=sources, module_name="test_fifo", hdl_toplevel="fifo")
