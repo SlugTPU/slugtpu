74e1f586a93cfd3d8f3875bedd0e043391053477
nappham <nappham@ucsc.edu>
2026-02-18 17:04:25 -0800

    updated pe.sv

--- rtl/pe.sv
+++ rtl/pe.sv
@@ -5,31 +5,33 @@ module pe #(
     input  logic                  clk_i,
     input  logic                  rst_i,
 
-    input  logic [DATA_WIDTH-1:0] act_in,
-    output logic [DATA_WIDTH-1:0] act_out,
+    input  logic [DATA_WIDTH+1:0] act_in,
+    output logic [DATA_WIDTH+1:0] act_out,
 
-    input  logic [DATA_WIDTH-1:0] weight_in,
-    output logic [DATA_WIDTH-1:0] weight_out,
-
-    input  logic                  buf_sel_in,
-    output logic                  buf_sel_out,
-
-    input  logic                  weight_we,
-    output logic                  weight_we_out,
+    input  logic [DATA_WIDTH+1:0] weight_in,
+    output logic [DATA_WIDTH+1:0] weight_out,
 
     input  logic [ACC_WIDTH-1:0]  psum_in,
     output logic [ACC_WIDTH-1:0]  psum_out
 );
 
-    logic [DATA_WIDTH-1:0] weight_buf [0:1];
+    logic sel;
+    logic valid;
+    logic [DATA_WIDTH-1:0] weight_data;
+
+    assign sel = weight_in[1];
+    assign valid = weight_in[0];
+    assign weight_data = weight_in[9:2];
+
+    logic [DATA_WIDTH+1:0] weight_buf [0:1];
 
     always_ff @(posedge clk_i) begin
-        if (weight_we)
-            weight_buf[~buf_sel_in] <= weight_in;
+        if (valid)
+            weight_buf[~sel] <= weight_data;
     end
 
     logic [DATA_WIDTH-1:0] active_weight;
-    assign active_weight = weight_buf[buf_sel_in];
+    assign active_weight = weight_buf[sel];
 
     always_ff @(posedge clk_i) begin
         if (rst_i)
@@ -38,11 +40,19 @@ module pe #(
             psum_out <= psum_in + (act_in * active_weight);
     end
 
+    logic prev_sel;
+    logic [DATA_WIDTH+1:0] prev_weight_in;
+
     always_ff @(posedge clk_i) begin
         act_out       <= act_in;
-        weight_out    <= weight_buf[~buf_sel_in];
-        buf_sel_out   <= buf_sel_in;
-        weight_we_out <= weight_we;
+        prev_sel      <= sel;
+        prev_weight_in <= weight_in;
+
+        if (prev_sel == sel)
+            weight_out <= weight_in;
+        else
+            weight_out <= old_weight_in; 
+        
     end
 
 endmodule
