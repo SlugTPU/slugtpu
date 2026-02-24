75c3666f768ffc12a122ea601196ae713014ae24
Shreyas Gupta <shreyas_gupta@outlook.com>
2026-02-23 19:39:06 -0800

    also fix elastic

--- rtl/utils/elastic.sv
+++ rtl/utils/elastic.sv
@@ -1,26 +1,30 @@
 module elastic
   #(parameter [31:0] width_p = 8
-   parameter [31:0] depth_p = 8
+   ,parameter [31:0] depth_p = 8
    )
   (input clk_i
   ,input rst_i
 
-  ,input [width_p - 1:0] data_i [depth_p-1:0]
+  ,input signed [width_p - 1:0] data_i [depth_p-1:0]
   ,input valid_i
   ,output ready_o 
 
   ,output logic valid_o 
-  ,output logic [width_p - 1:0] data_o [depth_p-1:0]
+  ,output logic signed [width_p - 1:0] data_o [depth_p-1:0]
   ,input ready_i
   );
-
-  always_ff @( posedge clk_i) begin
-    if (rst_i) begin
-      data_o <= '0;
-    end else if( valid_i && ready_o) begin
-      data_o <= data_i;
+  genvar i;
+  generate
+    for (i = 0; i < depth_p ; i++) begin
+      always_ff @( posedge clk_i) begin
+        if (rst_i) begin
+          data_o[i] <= '0;
+        end else if( valid_i && ready_o) begin
+          data_o[i] <= data_i[i];
+        end
+      end
     end
-  end
+  endgenerate
 
   always_ff @( posedge clk_i) begin
     if (rst_i) begin
