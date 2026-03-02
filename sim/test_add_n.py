d296c3e0bdf2f077f1b7a910b7cad56b90b39f7e
yanghuaxuan <yanghuaxuan@gmail.com>
2026-03-02 13:08:20 -0800

    add_n testing

--- sim/test_add_n.py
+++ sim/test_add_n.py
@@ -6,8 +6,10 @@ from pathlib import Path
 from shared import reset_sequence, clock_start, random_binary_driver
 from runner import run_test
 from collections import deque
-from cocotb.types import LogicArray, Logic
+from cocotb.types import LogicArray, Logic, Array
+import queue
 import random
+from shared import handshake
 
 def logic_add(a: LogicArray, b: LogicArray, width: int) -> LogicArray:
     """
@@ -18,11 +20,67 @@ def logic_add(a: LogicArray, b: LogicArray, width: int) -> LogicArray:
     res = (a.to_signed() + b.to_signed()) & (1 << width) - 1
     return LogicArray.from_signed(res, width)
 
+def add_n(data_i: Array[LogicArray], bias_i: Array[LogicArray], N: int, width: int) -> Array[LogicArray]:
+    """Adds N elements of data_i to bias_i."""
+    result = Array([], N)
+    for i in range(N):
+        result[i] = logic_add(data_i[i], bias_i[i], width)
+    return result
+
+# add_n is an elastic module that takes in N elements of data and bias, adds them together, and produces N elements of output.
+class add_n_model():
+    def __init__(self, N, width):
+        self.N = N
+        self.width = width
+        self.q = queue.SimpleQueue()
+
+    def consume(self, dut):
+        self.q.put((dut.data_i, dut.bias_i))
+
+    def produce(self, dut):
+        data_o = dut.data_o
+
+        got = data_o.value.to_signed()
+        expected = add_n(dut.data_i, dut.bias_i, self.N, self.width).to_signed()
+        assert got == expected, f"Expected {expected}, got {got}"
+
+class ModelRunner():
+    def __init__(self, dut):
+        self.dut = dut
+        self.model = add_n_model(dut.N, dut.width_p)
+
+    def start(self):
+        cocotb.start_soon(self.run_input())
+        cocotb.start_soon(self.run_output())
+
+    async def run_input(self):
+        clk_i = self.dut.clk_i
+        valid_i, valid_o = self.dut.data_valid_i, self.dut.data_valid_o
+        ready_i, ready_o = self.dut.data_ready_i, self.dut.data_ready_o
+        rst_i = self.dut.rst_i
+
+        while True:
+            await handshake(clk_i, rst_i, ready_o, valid_i)
+            await ReadWrite()
+            self.model.consume(self.dut)
+
+    async def run_output(self):
+        clk_i = self.dut.clk_i
+        valid_i, valid_o = self.dut.data_valid_i, self.dut.data_valid_o
+        ready_i, ready_o = self.dut.data_ready_i, self.dut.data_ready_o
+        rst_i = self.dut.rst_i
+
+        while True:
+            await handshake(clk_i, rst_i, ready_i, valid_o)
+            await ReadOnly()
+            self.model.produce(self.dut)
+
 @cocotb.test()
 async def reset_test(dut):
     """Test for Initialization"""
     clk_i = dut.clk_i
     rst_i = dut.rst_i
+    data_i = dut.data_i
 
     await clock_start(clk_i)
     await reset_sequence(clk_i, rst_i)
@@ -31,10 +89,29 @@ async def reset_test(dut):
 
 @cocotb.test()
 async def add_n_simple_test(dut):
+    clk_i = dut.clk_i
+    rst_i = dut.rst_i
+    bias_i = dut.bias_i
+    data_i = dut.data_i
+    data_valid_i = dut.data_valid_i
+    data_ready_i = dut.data_ready_i
+    N = dut.N
     width_p = dut.width_p
-    N = dut.N_p
 
-tests = ["reset_test", "load_simple_test"]
+    await clock_start(clk_i)
+    await reset_sequence(clk_i, rst_i)
+    await FallingEdge(rst_i)
+
+    data_ready_i.value = 1
+    data_valid_i.value = 1
+
+    for i in range(N.value):
+        bias_i[i].value = random.randint(-10, 10)
+        data_i[i].value = random.randint(-10, 10)
+
+    await RisingEdge(dut.clk_i)
+
+tests = ["reset_test", "add_n_simple_test"]
 proj_path = Path("./rtl").resolve()
 sources = [ proj_path/"utils/elastic.sv", proj_path/"scalar_units/add_n.sv"   ]
 
